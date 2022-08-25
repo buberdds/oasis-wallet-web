59f42c4a9ed990b515f4c077f013ae8a9b010660
Michal Zielenkiewicz <michal@oasisprotocol.org>
2022-08-25 11:17:56 +0200

    CR: fix account refreshing, types, deposit fee

--- src/app/state/account/saga.test.ts
+++ src/app/state/account/saga.test.ts
@@ -1,12 +1,19 @@
 import { testSaga } from 'redux-saga-test-plan'
 import { accountActions } from '.'
-import { accountSaga, fetchAccount, refreshAccountOnTransaction } from './saga'
+import {
+  accountSaga,
+  fetchAccount,
+  refreshAccountOnTransaction,
+  refreshAccountOnParaTimeTransaction,
+} from './saga'
 
 describe('Account Sagas', () => {
   test('accountSaga', () => {
     testSaga(accountSaga)
       .next()
       .fork(refreshAccountOnTransaction)
+      .next()
+      .fork(refreshAccountOnParaTimeTransaction)
       .next()
       .takeLatest(accountActions.fetchAccount, fetchAccount)
       .next()

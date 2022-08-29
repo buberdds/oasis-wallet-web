6c4a9e25abb6fbea8a089367eb106298525aa585
Michal Zielenkiewicz <michal@oasisprotocol.org>
2022-08-29 09:16:42 +0200

    Test refresh sagas

--- src/app/state/account/saga.test.ts
+++ src/app/state/account/saga.test.ts
@@ -1,4 +1,8 @@
-import { testSaga } from 'redux-saga-test-plan'
+import { expectSaga, testSaga } from 'redux-saga-test-plan'
+import { Wallet, WalletState } from 'app/state/wallet/types'
+import { stakingActions } from 'app/state/staking'
+import { transactionActions } from 'app/state/transaction'
+import { RootState } from 'types'
 import { accountActions } from '.'
 import {
   accountSaga,
@@ -18,5 +22,25 @@ describe('Account Sagas', () => {
       .takeLatest(accountActions.fetchAccount, fetchAccount)
       .next()
       .isDone()
+  })
+
+  it('Should refresh account on paraTime transaction', () => {
+    const address = 'oasis1qz0k5q8vjqvu4s4nwxyj406ylnflkc4vrcjghuwk'
+    return expectSaga(accountSaga)
+      .withState({
+        account: { address },
+        wallet: {
+          selectedWallet: 0,
+          wallets: [
+            {
+              address,
+            } as Partial<Wallet>,
+          ],
+        } as Partial<WalletState>,
+      } as Partial<RootState>)
+      .dispatch(transactionActions.paraTimeTransactionSent('dummyAddress'))
+      .put.actionType(accountActions.fetchAccount.type)
+      .put.actionType(stakingActions.fetchAccount.type)
+      .silentRun(50)
   })
 })

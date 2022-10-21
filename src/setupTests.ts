c1016b81f365e13370a5ad46b45b5fcad14649c8
Michal Zielenkiewicz <michal@oasisprotocol.org>
2022-10-21 15:39:46 +0200

    Dummy global XHR mock

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -24,3 +24,17 @@ global.fetch = require('portable-fetch')
 window.fetch = require('portable-fetch')
 
 global.window.scrollTo = () => {}
+
+const xhrMock: Partial<XMLHttpRequest> = {
+  abort: jest.fn(),
+  open: jest.fn(),
+  send: jest.fn(),
+  setRequestHeader: jest.fn(),
+  readyState: 4,
+  status: 200,
+  response: '',
+}
+
+beforeEach(() => {
+  jest.spyOn(window, 'XMLHttpRequest').mockImplementation(() => xhrMock as XMLHttpRequest)
+})
